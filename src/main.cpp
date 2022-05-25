9d11353f6fd845aaec7928bcb2772e0ba81348e5
PtiBouchon <lopite2@live.fr>
2022-05-25 17:54:14 +0200

    Basic Gravity simulation with Box2D

--- src/main.cpp
+++ src/main.cpp
@@ -13,19 +13,52 @@ int main()
 {
     RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "DownDownWeGo");
 
-    //Background
+    // ----- Background ----- //
     Texture bgTexture;
     bgTexture.loadFromFile("./resources/cave_bg.png");
     Sprite background(bgTexture);
 
-    //Player
-    Player player("./resources/Dude_Monster_Idle_4.png", 20, 10000, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2);
+    // ----- Physics ----- //
 
-    //Clock
+    // Define the gravity vector.
+    b2Vec2 gravity(0.0f, 10.0f);
+
+    // Construct a world object, which will hold and simulate the rigid bodies.
+    b2World world(gravity);
+
+    // Define the ground body.
+    b2BodyDef groundBodyDef;
+    groundBodyDef.position.Set(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2);
+
+    // Call the body factory which allocates memory for the ground body
+    // from a pool and creates the ground box shape (also from a pool).
+    // The body is also added to the world.
+    b2Body* groundBody = world.CreateBody(&groundBodyDef);
+
+    // Define the ground box shape.
+    b2PolygonShape groundBox;
+
+    // The extents are the half-widths of the box.
+    groundBox.SetAsBox(50.0f, 10.0f);
+
+    // Add the ground fixture to the ground body.
+    groundBody->CreateFixture(&groundBox, 0.0f);
+
+    // ----- Player ----- //
+    Player player("./resources/Dude_Monster_Idle_4.png", 20, 10000, WINDOW_WIDTH / 2, 0, &world);
+
+    // ----- Clock ----- //
     Clock clock;
     double time = clock.getElapsedTime().asSeconds();
     double oldtime;
     double deltaTime;
+
+    // Prepare for simulation. Typically we use a time step of 1/60 of a
+    // second (60Hz) and 10 iterations. This provides a high quality simulation
+    // in most game scenarios.
+    float timeStep = 1.0f / 60.0f;
+    int32 velocityIterations = 6;
+    int32 positionIterations = 2;
 
     while (window.isOpen())
     {
@@ -40,6 +73,8 @@ int main()
                 window.close();
             }
         }
+
+        world.Step(timeStep, velocityIterations, positionIterations);
 
         window.clear();
         
--- src/player.h
+++ src/player.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <stdio.h>
 #include <SFML/Graphics.hpp>
+#include "box2d/box2d.h"
 
 class Player {
 private:
@@ -9,16 +10,18 @@ private:
 
     double baseSpeed;
 
+    b2Body* body;
+
 public:
     int health;
     bool idle;
 
-    double x_pos;
-    double y_pos;
+//    double x_pos;
+//    double y_pos;
     double xSpeed;
     double ySpeed;
 
-    Player(std::string image, int health, double baseSpeed, double xpos, double ypos);
+    Player(std::string image, int health, double baseSpeed, double xpos, double ypos, b2World *world);
     sf::Sprite getSprite();
 
     void Move(sf::Event::KeyEvent key);
