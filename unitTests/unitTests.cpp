cc8c83048ba84363eff2e6da03f555b44c8afdd1
PtiBouchon <lopite2@live.fr>
2022-06-12 23:42:47 +0200

    Added one unitTests !

--- unitTests/unitTests.cpp
+++ unitTests/unitTests.cpp
@@ -1,6 +1,21 @@
 #include <gtest/gtest.h>
+#include <box2d/box2d.h>
+#include <SFML/Graphics.hpp>
+#include "../src/Tile/myTilemap.h"
 
 TEST(TestGroupName, TestName)
 {
+    EXPECT_EQ(true, true);
+}
+
+TEST(TestTilemap, Test1)
+{
+    tmx::Map map;
+    map.load("../../resources/maps/map1.tmx");
+
+    b2World world(b2Vec2(0, 10));
+
+    MyTilemap tilemap(map, &world, sf::Vector2f(0, 0));
+
     EXPECT_EQ(true, true);
 }
