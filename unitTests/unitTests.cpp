d26aa6502c160b67e6ec5414b1a54c2783538e40
PtiBouchon <lopite2@live.fr>
2022-06-13 00:02:59 +0200

    A new unit test !

--- unitTests/unitTests.cpp
+++ unitTests/unitTests.cpp
@@ -2,20 +2,25 @@
 #include <box2d/box2d.h>
 #include <SFML/Graphics.hpp>
 #include "../src/Tile/myTilemap.h"
+#include "../src/Tile/tilemapManager.h"
 
-TEST(TestGroupName, TestName)
-{
-    EXPECT_EQ(true, true);
-}
-
-TEST(TestTilemap, Test1)
+TEST(TestTilemap, TestCreate)
 {
     tmx::Map map;
-    map.load("../../resources/maps/map1.tmx");
+    bool loaded = map.load("../../resources/maps/map1.tmx");
+    ASSERT_EQ(loaded, true);
 
     b2World world(b2Vec2(0, 10));
 
-    MyTilemap tilemap(map, &world, sf::Vector2f(0, 0));
+    EXPECT_NO_THROW(MyTilemap(map, &world, sf::Vector2f(0, 0)));
+}
 
-    EXPECT_EQ(true, true);
+TEST(TestTilemapManager, TestMapSize)
+{
+    b2World world(b2Vec2(0, 10));
+
+    TilemapManager tilemapManager("../../resources/maps/", &world);
+
+    ASSERT_EQ(tilemapManager.getMapWidth(), 320);
+    ASSERT_EQ(tilemapManager.getMapHeight(), 256);
 }
